82c705aefe59b04218751a2a70de028bf1e68c1b
Almosk <alvin.kita@outlook.fr>
2024-04-25 10:03:13 +0200

    init project

---
+++ src/main/java/com/ipi/gestiondechampionnatapi/GestionDeChampionnatApiApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.gestiondechampionnatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeChampionnatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeChampionnatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipi/gestiondechampionnatapi/GestionDeChampionnatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipi.gestiondechampionnatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeChampionnatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
